ffa999c7dd714a12a03fef754f04a29318c131f7
Quentin Boyer <Quentin.Boyer@ensimag.fr>
2018-11-25 00:08:06 +0100

    Added networkable types

--- include/networkable.hpp
+++ include/networkable.hpp
@@ -4,50 +4,140 @@
 #include <string>
 #include <vector>
 
-/// \brief Object to be send to another Node in a byte list
-class Networkable{
-	public:
-		/// \brief Get the binary string represesentation
-		/// of the Networkable object
-		std::string asBytes() const;
-		/// \brief Calculate the hex string representing
-		/// the object
-		virtual std::string byteRepr() const = 0;
-		virtual ~Networkable() = 0;
-};
+/// \brief Basic types sendables as bytes by TCP
+namespace networkable{
 
-/// \brief Networkable unsigned int of variable length
-/// \details Size is either 2,3,5 or 9 bytes depending on
-/// the value of the integer. Prefixes to indicate length are
-/// 2:No Prefix, 3:fd, 5:fe, 9:ff
-class Var_uint : public Networkable{
-	protected:
-		/// \brief Value of the 64 bit integer
-		uint64_t value;
-	public:
-		/// \brief Return the uint64 value
-		uint64_t getValue() const;
-		/// \brief Representation as packed hex string
-		std::string byteRepr() const override;
-		/// \brief Create a Var_uint
-		explicit Var_uint(uint64_t val);
-		/// \brief Read a Var_uint from a binary reprentation
-		explicit Var_uint(const std::string& binaryString);
-};
+	/// \brief Object to be send to another Node in a byte list
+	class Networkable{
+		public:
+			/// \brief Get the binary string represesentation
+			/// of the Networkable object
+			std::string asBytes() const;
+			/// \brief Get the represention of the object
+			/// as a packed hexadecimal string
+			virtual std::string byteRepr() const = 0;
+			virtual ~Networkable() = 0;
+	};
 
-/// \brief Networkable string of variable length
-class Var_str : public Networkable{
-	protected:
-		std::string value;
-	public:
-		/// \brief Returns the underlying value
-		std::string getValue() const;
-		/// \brief Representation as packed hex string
+	/// \brief Networkable unsigned int of variable length
+	/// \details Size is either 2,3,5 or 9 bytes depending on
+	/// the value of the integer. Prefixes to indicate length are
+	/// 2:No Prefix, 3:fd, 5:fe, 9:ff
+	class Var_uint : public Networkable{
+		protected:
+			/// \brief Value of the at most 64 bit integer
+			uint64_t value;
+		public:
+			/// \brief Return the uint64 value
+			uint64_t getValue() const;
+			std::string byteRepr() const override;
+			/// \brief Create a Var_uint
+			explicit Var_uint(uint64_t val);
+	};
+
+	/// \brief Networkable uint16
+	class Uint16 : public Networkable{
+		protected:
+			/// \brief value of the 16 bit uint
+			uint16_t value;
+		public:
+			/// \brief Return the value
+			uint16_t getValue() const;
+			std::string byteRepr() const override;
+			/// \brief Create a Uint16
+			explicit Uint16(uint16_t val);
+	};
+	/// \brief Networkable uint32
+	class Uint32 : public Networkable{
+		protected:
+			/// \brief Value of the uint32 
+			uint32_t value;
+		public:
+			/// \brief Return the value
+			uint32_t getValue() const;
+			std::string byteRepr() const override;
+			/// \brief Create a Uint32
+			explicit Uint32(uint32_t val);
+	};
+	/// \brief Networkable uint64
+	class Uint64 : public Networkable{
+		protected:
+			/// \brief Value of the uint64
+			uint64_t value;
+		public:
+			/// \brief Return the value
+			uint64_t getValue() const;
+			std::string byteRepr() const override;
+			/// \brief Create a Uint32
+			explicit Uint64(uint64_t val);
+	};
+
+	/// \brief Networkable string of variable length
+	class Var_str : public Networkable{
+		protected:
+			/// \brief Value of the string
+			std::string value;
+		public:
+			/// \brief Returns the underlying value
+			std::string getValue() const;
+			std::string byteRepr() const override;
+			/// \brief Create a Var_str
+			explicit Var_str(const std::string& val);
+	};
+	
+	/// \brief Ressource reference (hash and type)
+	struct Inv_vect : public Networkable{
+		/// \brief Possible ressources
+		enum ressource_type { blockRes, txRes, invalidRes };
+
+		/// \brief ressource_type of the Inv_vect
+		ressource_type type;
+		/// \brief hash of the ressource_type
+		std::string hash;
+
+		/// \brief Construct an Inv_vect from fields
+		Inv_vect(ressource_type res, 
+				std::string resHash);
+
 		std::string byteRepr() const override;
-		/// \brief Create a Var_str
-		explicit Var_str(std::string val, bool);
-		/// \brief Construct a Var_str from a binary string
-		explicit Var_str(const std::string& binaryString);
-};
+	};
+	
+	/// \brief Network address of a known Node
+	class Address : public Networkable{
+		private:
+			/// \brief Last time the Node was seen
+			uint64_t timestamp;
+			/// \brief IPv6 address of the Node
+			std::string address;
+			/// \brief Port used by the Node
+			uint16_t port;
+		public:
+			/// \brief Returns the address
+			std::string getAddress() const;
+			/// \brief Returns the port
+			uint16_t getPort() const;
+			/// \brief Returns the time since the last known activity
+			uint64_t getTimestamp() const;
+			/// \brief Construct an Address
+			Address(uint64_t lastActive, std::string ipAddress, uint16_t prt);
+			std::string byteRepr() const override;
+	};
+
+	/// \brief A header for a message::Message
+	struct MessageHeader : public Networkable{
+		/// \brief The message::Message magic number
+		uint32_t magic;
+		/// \brief The message::Message type
+		std::string type;
+		/// \brief The Length of the message::Message payload
+		uint64_t payloadLength;
+		
+		/// \brief Construct a MessageHeader
+		MessageHeader(uint32_t _magic,std::string _type,
+				uint64_t pldSize);
+		std::string byteRepr() const override;
+	};
+
+} //namespace networkable
 
 #endif /* NETWORKABLE_HPP */
--- src/networkable.cpp
+++ src/networkable.cpp
@@ -2,102 +2,136 @@
 #include "util.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <cryptopp/hex.h>
 #include <sstream>
 
-Networkable::~Networkable() {}
+namespace networkable{
 
-std::string Networkable::asBytes() const{
-	std::string hexRepr = this->byteRepr();
-	std::string decodedBytes;
+	Networkable::~Networkable() {}
+
+	std::string Networkable::asBytes() const{
+		std::string hexRepr = this->byteRepr();
+		std::string decodedBytes;
+
+		CryptoPP::StringSource ss(hexRepr, true,
+				new CryptoPP::HexDecoder(
+					new CryptoPP::StringSink(decodedBytes)
+					) // HexDecoder
+				); // String Source
+		return decodedBytes;
+	}
+
+	Uint16::Uint16(uint16_t val) : value(val) {}
+
+	std::string Uint16::byteRepr() const{
+		std::ostringstream sstream;
+		sstream << std::setfill('0')
+			<< std::setw(4) 
+			<< std::hex 
+			<< value;
+		return sstream.str();
+	}
+	uint16_t Uint16::getValue() const{
+		return value;
+	}
+
+	Uint32::Uint32(uint32_t val) : value(val) {}
 	
-	CryptoPP::StringSource ss(hexRepr, true,
-		new CryptoPP::HexDecoder(
-			new CryptoPP::StringSink(decodedBytes)
-		) // HexDecoder
-	); // String Source
-	return decodedBytes;
-}
+	std::string Uint32::byteRepr() const{
+		std::ostringstream sstream;
+		sstream << std::setfill('0')
+			<< std::setw(8)
+			<< std::hex
+			<< value;
+		return sstream.str();
+	}
+	uint32_t Uint32::getValue() const{
+		return value;
+	}
 
-Var_uint::Var_uint(uint64_t val) : value(val) {}
+	Uint64::Uint64(uint64_t val) : value(val) {}
+	std::string Uint64::byteRepr() const{
+		std::ostringstream sstream;
+		sstream << std::setfill('0')
+			<< std::setw(16)
+			<< std::hex
+			<< value;
+		return sstream.str();
+	}
+	uint64_t Uint64::getValue() const{
+		return value;
+	}
 
-Var_uint::Var_uint(const std::string& binaryString){
-	if ( binaryString[0] < char(0xfd) ){
-		value = binaryString[0];
+	Var_uint::Var_uint(uint64_t val) : value(val) {}
+
+	uint64_t Var_uint::getValue() const{
+		return value;
 	}
-	else if ( binaryString[0] == char(0xfd)){
-		value = ((unsigned char)(binaryString[1]) << 8) |
-			((unsigned char)(binaryString[2]));
-	}
-	else if ( binaryString[0] == char(0xfe)){
-		value = ((unsigned char)(binaryString[1]) << 24) |
-			((unsigned char)(binaryString[2]) << 16) |
-			((unsigned char)(binaryString[3]) << 8)  |
-			((unsigned char)(binaryString[4]));
-	}
-	else{
-		for(int i = 2; i < 9; ++i){
-			auto byte = (unsigned char)(binaryString[i]);
-			value |= (uint64_t)(byte) << 8*(8-i);
+
+	std::string Var_uint::byteRepr() const{
+		std::ostringstream sstream;
+		sstream << std::hex << value;
+		auto rawStr = sstream.str();
+		if(value <= 252){
+			return std::string(2-rawStr.length(), '0') + rawStr;
 		}
-		binaryString[9];
-	}
-}
-
-uint64_t Var_uint::getValue() const{
-	return value;
-}
-
-std::string Var_uint::byteRepr() const{
-	std::ostringstream sstream;
-	sstream << std::hex << value;
-	auto rawStr = sstream.str();
-	if(value <= 252){
-		return std::string(2-rawStr.length(), '0') + rawStr;
-	}
-	if(value <= 0xffff){
-		return "fd" + 
-		       std::string(4-rawStr.length(),'0') + 
-		       rawStr;
-		// 3 : 0xFD.value
-	}
-	if(value <= 0xffffffff){
-		return "fe" +
-			std::string(8-rawStr.length(),'0') +
+		if(value <= 0xffff){
+			return "fd" + 
+				std::string(4-rawStr.length(),'0') + 
+				rawStr;
+			// 3 : 0xFD.value
+		}
+		if(value <= 0xffffffff){
+			return "fe" +
+				std::string(8-rawStr.length(),'0') +
+				rawStr;
+			// 5 : 0xFE.value
+		}
+		return "ff" +
+			std::string(16-rawStr.length(),'0') +
 			rawStr;
-		// 5 : 0xFE.value
+		// 9 0xFF.value
 	}
-	return "ff" +
-	       std::string(16-rawStr.length(),'0') +
-	       rawStr;
-	// 9 0xFF.value
-}
 
-Var_str::Var_str(std::string val, bool) : value(val) {}
+	Var_str::Var_str(const std::string& val) : value(val) {}
 
-Var_str::Var_str(const std::string& binaryString){
-	if(binaryString[0] == char(0xfd)){
-		value = binaryString.substr(3);
+	std::string Var_str::getValue() const{
+		return value;
 	}
-	else if(binaryString[0] == char(0xfe)){
-		value = binaryString.substr(5);
+	std::string Var_str::byteRepr() const{
+		std::ostringstream bss;
+		bss << Var_uint(value.size()).byteRepr();
+		for(auto& chr : value){
+			bss << util::hex(chr);
+		}
+		return bss.str();
 	}
-	else if(binaryString[0] == char(0xff)){
-		value = binaryString.substr(9);
-	}
-	else{
-		value = binaryString.substr(1);
-	}
-}
 
-std::string Var_str::getValue() const{
-	return value;
-}
-std::string Var_str::byteRepr() const{
-	std::ostringstream bss;
-	bss << Var_uint(value.size()).byteRepr();
-	for(auto& chr : value){
-		bss << util::hex(chr);
+	Inv_vect::Inv_vect(Inv_vect::ressource_type res, 
+			std::string resHash) : 
+		type(res),
+		hash(resHash) {}
+
+	std::string Inv_vect::byteRepr() const{
+		if( type == invalidRes ){
+			std::cerr << "Cannot create bytes of invalidRes" << std::endl;
+			return "INVALID RESSOURCE";
+		}
+		return Uint32(type).byteRepr() + hash;
 	}
-	return bss.str();
-}
+
+	MessageHeader::MessageHeader(uint32_t _magic, 
+			std::string _type,
+			uint64_t pldSize) : 
+		magic(_magic),
+		type(_type),
+		payloadLength(pldSize) {}
+	std::string MessageHeader::byteRepr() const{
+		std::string out = Uint32(magic).byteRepr();
+		out += type;
+		out += Var_uint(payloadLength).byteRepr();
+		return out;
+	}
+
+} // namespace networkable
